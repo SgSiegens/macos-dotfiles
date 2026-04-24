a8246953e150dcd21205a15537a43530e0a02a7b
SgSiegens <uxgzc@student.kit.edu>
2026-04-24 15:43:17 +0200

    kitty now instanlty changes when the wallpaperscript menu gets executed. for some reason skhd has no access to the PATH variable

--- scripts/pywal16.sh
+++ scripts/pywal16.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 ln -sf ~/.cache/wal/flameshot.ini ~/dotfiles/.config/flameshot/flameshot.ini
+
+for socket in /tmp/kitty*; do
+    # Check if the file is actually a socket (-S) to prevent errors
+    if [ -S "$socket" ]; then
+        # Push the new colors to this specific Kitty instance
+        /Applications/kitty.app/Contents/MacOS/kitty @ --to "unix:$socket" set-colors -a ~/.cache/wal/colors-kitty.conf
+    fi
+done
