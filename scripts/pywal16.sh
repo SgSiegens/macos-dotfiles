93ba6932ebfc9327783a864ae962100ce9bfec42
SgSiegens <uxgzc@student.kit.edu>
2026-04-25 10:33:43 +0200

    cleaned up some stuff

--- scripts/pywal16.sh
+++ scripts/pywal16.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# symlink all templates so that the colors get adopted
 ln -sf ~/.cache/wal/flameshot.ini ~/dotfiles/.config/flameshot/flameshot.ini
 
+# update the simple-bar colors via their provided script
+"$HOME/ubersicht/widgets/simple-bar/lib/styles/pywal/pywal-gen.sh"
+
+# update all kitty terminals
 for socket in /tmp/kitty*; do
-    # Check if the file is actually a socket (-S) to prevent errors
+    # Check if the file is actually a socket to prevent errors
     if [ -S "$socket" ]; then
-        # Push the new colors to this specific Kitty instance
         /Applications/kitty.app/Contents/MacOS/kitty @ --to "unix:$socket" set-colors -a ~/.cache/wal/colors-kitty.conf
     fi
 done
-
-/Users/thm/dotfiles/.config/ubersicht/widgets/simple-bar/lib/styles/pywal/pywal-gen.sh
