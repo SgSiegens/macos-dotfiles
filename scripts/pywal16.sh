164463c5ed0be567e21b6287ab20d78d8c5457af
SgSiegens <uxgzc@student.kit.edu>
2026-04-25 19:21:55 +0200

    needed to make my own color template since when using the one provided by pywal the theme switching was too slow

--- scripts/pywal16.sh
+++ scripts/pywal16.sh
@@ -15,8 +15,10 @@ if [ -f "$SB_WIDGET/lib/styles/pywal/pywal-gen.sh" ]; then
     bash "$SB_WIDGET/lib/styles/pywal/pywal-gen.sh"
 fi
 
+
+
 # Inject colors into .simplebarrc via jq
-if [ -f "$WAL_CACHE/colors.sh" ]; then
+if [ -f "$WAL_CACHE/mycolors.sh" ]; then
     # load generated color palette
     source "$WAL_CACHE/colors.sh"
 
@@ -38,7 +40,7 @@ if [ -f "$WAL_CACHE/colors.sh" ]; then
       --background: $background;
       --transparent-dark: rgba(0, 0, 0, 0.05);
       --font: \"JetBrains Mono\", Monaco, Menlo, monospace;
-      --font-size: 11px;
+      --font-size: 11.5px;
       --bar-height: 34px;
       --bar-radius: 6px;
       --bar-border: 0px solid transparent;
@@ -49,7 +51,7 @@ if [ -f "$WAL_CACHE/colors.sh" ]; then
       --item-outer-margin: 0 0 0 5px;
       --hover-ring: 0 0 0 2px rgba(255, 255, 255, 0.75);
       --focus-ring: 0 0 0 2px rgb(255, 255, 255);
-      --light-shadow: 0 5px 10px rgba(0, 0, 0, 0.24);
+      --light-shadow: 0 5px 10px rgba(0, 0, 0, 0.4);
       --foreground-shadow: 0 0 0 1px var(--foreground);
       --transition-easing: cubic-bezier(0.4, 0, 0.2, 1);
       --click-effect: rgba(255, 255, 255, 0.3);
