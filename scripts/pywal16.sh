6534987e8b480e1e462118ae3a53bdbaf40a1d7e
SgSiegens <uxgzc@student.kit.edu>
2026-05-06 22:37:32 +0200

    completly switched to a template alternative for making pywal work with simplebar

--- scripts/pywal16.sh
+++ scripts/pywal16.sh
@@ -11,8 +11,14 @@ ln -sf "$WAL_CACHE/flameshot.ini" "$DOTFILES/.config/flameshot/flameshot.ini"
 ln -sf "$WAL_CACHE/pywal_theme.js" "$SB_WIDGET/lib/styles/themes/pywal_theme.js"
 ln -sf "$WAL_CACHE/bordersrc" "$DOTFILES/.config/borders/bordersrc"
 
+# Simplebar needs some addtional handling
+if [ -f "$SB_WIDGET/lib/styles/pywal/pywal-gen.sh" ]; then
+    bash "$SB_WIDGET/lib/styles/pywal/pywal-gen.sh"
+fi
+ln -sf "$WAL_CACHE/.simplebarrc" "$HOME/.simplebarrc"
+
 # update simplebarrc
-bash ~/scripts/update_simplebar.sh
+# bash ~/scripts/update_simplebar.sh
 
 osascript -e 'tell application id "tracesOf.Uebersicht" to refresh'
 
